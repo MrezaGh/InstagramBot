af35d0ba5042bde0989cf2283cec227408016650
Mreza Gh <mrezagh.legend@gmail.com>
2020-08-15 00:01:33 +0430

    :fire: avoid getting banned by instagram

--- instapy_bot.py
+++ instapy_bot.py
@@ -4,6 +4,7 @@ from instapy import InstaPy
 class InstaPyBot:
     def __init__(self, username, password):
         self.session = InstaPy(username=username, password=password)
+        self.session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
 
     def login(self):
         self.session.login()
