bdf15962491beaa23557c7d381c628a8a980ce0b
Mreza Gh <mrezagh.legend@gmail.com>
2020-08-15 00:22:16 +0430

    :no_entry_sign: remove access to bot in bot.py file

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ from instapy_bot import InstaPyBot
 from secrets import instagram_username, instagram_pass
 
 
-bot = InstaPyBot(instagram_username, instagram_pass)
+InstaPyBot(instagram_username, instagram_pass).login()
 
-bot.login()
 
 
